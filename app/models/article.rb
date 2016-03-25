8e71bf11d27bc4afde083471f22898ec77b51d0c
lunchableavenger <santonrod4@gmail.com>
2016-03-25 14:34:39 -0400

    Update article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,11 @@
 class Article < ActiveRecord::Base
   validates :phone_number, presence: true,
-                    length: { is: 11 }
+                    length: { is: 11, message "Must be 11 digits long" }
 
 
   validates :phone_number, numericality: { only_integer: true }
+  
+  validates :message, presence: true
 
 
   #validates :legacy_code, format: { /^(?<num>\d+)$/,
